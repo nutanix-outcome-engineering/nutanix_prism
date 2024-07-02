d3be5c6aba0a549a82c1871dfa8f6efa39ed869c
Will Whitt <william.whitt@nutanix.com>
2024-07-02 15:33:43 -0400

    whoops wrong package name

--- bin/veracode.sh
+++ bin/veracode.sh
@@ -2,4 +2,4 @@
 
 wget https://downloads.veracode.com/securityscan/pipeline-scan-LATEST.zip -O pipeline-scan-latest.zip
 unzip -n pipeline-scan-latest.zip
-java -jar pipeline-scan.jar --veracode_api_id ${VERACODE_ID} --veracode_api_key ${VERACODE_TOKEN} --file nutanix_foundation.tar.gz
+java -jar pipeline-scan.jar --veracode_api_id ${VERACODE_ID} --veracode_api_key ${VERACODE_TOKEN} --file prism_api.tar.gz
